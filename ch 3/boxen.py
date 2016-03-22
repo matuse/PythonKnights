1bdbd4db72417a9c8eaef1822472e6f68806700a
matuse <matuse@gmail.com>
2016-03-21 20:09:27 -0600

    ch3 box example - extended

--- ch 3/boxen.py
+++ ch 3/boxen.py
@@ -18,3 +18,25 @@ def draw_box():
     draw_top()
 
 draw_box()
+
+def big_box_top():
+    print('+----+----+----+----+')
+
+def big_box_mid():
+    print('|    |    |    |    |')
+
+def draw_big_section():
+    big_box_top()
+    big_box_mid()
+    big_box_mid()
+    big_box_mid()
+    big_box_mid()
+
+def draw_big_box():
+    draw_big_section()
+    draw_big_section()
+    draw_big_section()
+    draw_big_section()
+    big_box_top()
+
+draw_big_box()
