1f175aa0c54e033e4124305f5beb1f16c91a55a1
matuse <matuse@gmail.com>
2016-03-21 20:55:12 -0600

    cleaning up styles

--- ch 3/boxen2.py
+++ ch 3/boxen2.py
@@ -2,16 +2,16 @@ width = 2
 height = 2
 
 
-def draw_div(width):
-    return'+----' * width + '+\n'
+def draw_div(w):
+    return'+----' * w + '+\n'
 
 
-def draw_mid(width):
-    return'|    ' * width + '|\n'
+def draw_mid(w):
+    return'|    ' * w + '|\n'
 
 
-def draw_section(width):
-    return draw_div(width) + draw_mid(width)*4
+def draw_section(w):
+    return draw_div(w) + draw_mid(w)*4
 
 
 print (draw_section(width) * height + draw_div(width))
