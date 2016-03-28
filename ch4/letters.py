6b1ff1032927a5f7e773def3fba2499fd0358f54
Matuse <matuse@gmail.com>
2016-03-27 22:46:59 -0600

    Starting an alphabet

---
+++ ch4/letters.py
@@ -0,0 +1,42 @@
+from polygon import *
+
+
+def draw_a(t, size):
+    lt(t, 60)
+    fd(t, size)
+    rt(t, 120)
+    fd(t, size)
+    lt(t, 60)
+
+
+def draw_b(t, size):
+    lt(t)
+    fd(t, size)
+    bk(t, size)
+    rt(t)
+    arc(t, (size / 4), 180)
+    rt(t, 180)
+    arc(t, (size / 4), 180)
+    lt(t)
+    fd(t, size)
+    lt(t)
+    pu(t)
+    fd(t, size/2)
+    pd(t)
+
+
+def draw_c(t, size):
+    pu(t)
+    fd(t, size/2)
+    lt(t)
+    fd(t, size)
+    rt(t)
+    rt(t, 180)
+    pd(t)
+    arc(t, size/2, 180)
+
+
+def skip(t, size):
+    pu(t)
+    fd(t, size)
+    pd(t)
